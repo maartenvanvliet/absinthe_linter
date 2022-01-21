42d90ed5387d6d11ea7c388b86f46afd672b9f5d
Maarten van Vliet <maartenvanvliet@gmail.com>
2022-01-21 08:43:44 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AbsintheLinter.MixProject do
   def project do
     [
       app: :absinthe_linter,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
